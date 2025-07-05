5a5cd2e4d39ed9d720cf54f87402578f2653f679
AnythingZen <chengzhanxd@gmail.com>
2025-07-05 22:45:44 +0800

    add cron job to refresh server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db.js";
+import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+
 import transactionsRoute from "./routes/transactionsRoute.js";
-import cors from "cors";
+import job from "./config/cron.js";
 
 dotenv.config();
+
 const app = express();
-app.use(cors());
+if (process.env.NODE_ENV === "production") job.start();
 
 // Middleware
 app.use(express.json()); // to parse into json from bit streams
@@ -15,25 +17,10 @@ app.use(rateLimiter); // check times user requested & limits accordingly
 
 const PORT = process.env.PORT || 5001;
 
-// @TODO: Can add a healthcheck route on API
-
-const initDB = async () => {
-	try {
-		await sql`CREATE TABLE IF NOT EXISTS transactions (
-            id SERIAL PRIMARY KEY,
-            user_id VARCHAR(255) NOT NULL,
-            title VARCHAR(255) NOT NULL,
-            amount DECIMAL(10, 2) NOT NULL,
-            category VARCHAR(255) NOT NULL,
-            created_at DATE NOT NULL DEFAULT CURRENT_DATE
-        )`;
-
-		console.log("Database initialized successfully.");
-	} catch (error) {
-		console.log("Error initializing database:", error);
-		process.exit(1);
-	}
-};
+// healthcheck route on API
+app.get("/api/health", (req, res) => {
+	res.status(200).json({ status: "ok" });
+});
 
 app.use("/api/transactions", transactionsRoute);
 
